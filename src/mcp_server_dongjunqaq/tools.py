b724b82334b8fb764307ee94c95c30b4bc9af4d1
DongJun <you@example.com>
2025-08-06 21:12:38 +0800

    feat: 添加获取平台相关信息的功能

--- src/mcp_server_dongjunqaq/tools.py
+++ src/mcp_server_dongjunqaq/tools.py
@@ -1,5 +1,7 @@
 import platform
 
+import psutil
+
 from src.mcp_server_dongjunqaq import my_mcp
 
 
@@ -7,7 +9,12 @@ from src.mcp_server_dongjunqaq import my_mcp
 def get_platform_info() -> dict:
     """获取平台的相关信息"""
     platform_info: dict[str:str] = {
-        "system": platform.system(),
-        "release": platform.release()
+        "操作系统": platform.system(),
+        "系统发行版本": platform.release(),
+        "系统版本信息": platform.version(),
+        "平台网络名": platform.node(),
+        "平台架构": platform.machine(),
+        "CPU信息": platform.processor(),
+        "总内存(GB)": round(psutil.virtual_memory().total / (1024 ** 3), 2),  # round(...,2)将一个数四舍五入并保留2位小数
     }
     return platform_info
